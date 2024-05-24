b325ddd2b77c7bf3ab8c1cf303b2d3b876e38c91
Brandon M. Smith <bmsmith@ps.dps.state.oh.us>
2024-05-24 13:55:10 -0400

    hopefully fixed

--- config.py
+++
@@ -1,4 +0,0 @@
-BASE_POINTS = 500
-CLAIM_POINTS = BASE_POINTS // 2
-CLAIM_COOLDOWN_PERIOD = 604800  # Cooldown period for claim command in seconds (1 week)
-LOAN_INTEREST_RATE = 0.1  # 10% interest on loans
