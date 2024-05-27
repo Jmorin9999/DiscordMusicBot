613511823f0d6f86b70d7c2fc6e2728f7f37c4f3
Brandon <brandonmsmith00@gmail.com>
2024-05-27 10:44:26 -0400

    change file location

--- bot/main.py
+++ bot/main.py
@@ -17,9 +17,9 @@ logging.basicConfig(level=logging.INFO)
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 # Load data files
-POINTS_FILE = '/var/tmp/discord/user_points.json'
-BET_HISTORY_FILE = '/var/tmp/discord/bet_history.json'
-LOANS_FILE = '/var/tmp/discord/loans.json'
+POINTS_FILE = '/home/ansible/discord/user_points.json'
+BET_HISTORY_FILE = '/home/ansible/discord/bet_history.json'
+LOANS_FILE = '/home/ansible/discord/loans.json'
 
 user_points = load_json(POINTS_FILE)
 bet_history = load_json(BET_HISTORY_FILE)
